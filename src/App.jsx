8a4b999b2b7e6b666dd5d41ac9d796c7cf231a6d
Srujanpola <srujanpola25@gmail.com>
2025-04-01 19:23:08 +0530

    Added Login Comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import { useState } from "react";
-import { useGoogleLogin } from "@react-oauth/google";
 import "./App.css";
-import axios from "axios";
-import { useEffect } from "react";
 function App() {
-
-  const [accessToken,setAccessToken] = useState('')
-  const handleGoogleSignIn = useGoogleLogin({
-    onSuccess: (response) => {
-      console.log(response.access_token)
-      setAccessToken(response.access_token);
-    }
-  })
-
-  useEffect(() => {
-    if(accessToken){
-      getUserDetails();
-    }
-  },[accessToken])
-
-  const getUserDetails =async () =>{
-
-    const response  = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
-      headers: {
-        Authorization:`Bearer ${accessToken}`
-      }
-    })
-    
-    console.log("google Response",response);
-  }
 
   return (
     <div>
       <h1>Workout Bros</h1>
-      <button onClick={handleGoogleSignIn}>Sign in with google</button>
     </div>
   );
 }
