3262ad09c012b9cab18812588abd3edad68a18d6
Srujanpola <srujanpola25@gmail.com>
2025-04-01 17:18:10 +0530

    Addded Google Authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+ <GoogleOAuthProvider clientId='151193059285-r9d8q97is7qo0cgutufq2ndhld5mt78d.apps.googleusercontent.com'>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </GoogleOAuthProvider>
 )
