519cfe0b852a1d4d8cdef11be8d56b3e161f4d65
Srujanpola <srujanpola25@gmail.com>
2025-04-02 16:37:14 +0530

    fixed few issues

--- src/common/routing.jsx
+++ src/common/routing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import Login from "../pages/login/Login";
-import Profile from "../pages/register/CreateProfile";
+import CreateProfile from "../pages/register/CreateProfile";
 import Explore from "../pages/explore/Explore";
 import Profile from "../pages/profile/Profile";
 
@@ -11,10 +11,9 @@ const Routing = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/CreateProfileInfo" element={<CreateProfileInfo />} />
+        <Route path="/CreateProfile" element={<CreateProfile />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
