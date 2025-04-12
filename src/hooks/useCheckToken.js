ef1f83990733cf178c29723ffb4416dc6eaaf969
Archit-KineticScape <archit@kineticscapestudios.com>
2025-04-12 16:43:02 +0530

    Update useCheckToken.js

--- src/hooks/useCheckToken.js
+++ src/hooks/useCheckToken.js
@@ -6,35 +6,35 @@ import { getnearbyusersapi } from "../common/apis";
 const useCheckToken = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const checkToken = async () => {
-      const token = localStorage.getItem("token");
-      const tokenData = JSON.parse(token);
+  const checkToken = async () => {
+    const token = localStorage.getItem("token");
+    const tokenData = JSON.parse(token);
 
-      console.log("hook token :", tokenData);
+    console.log("hook token :", tokenData);
 
-      if (!tokenData) {
-        navigate("/");
-      } else {
-        try {
-          const response = await axios.get(getuserprofileapi, {
-            headers: {
-              Authorization: `Bearer ${tokenData}`,
-            },
-          });
+    if (!tokenData) {
+      navigate("/");
+    } else {
+      try {
+        console.log("Calling the get user api");
+        const response = await axios.get(getuserprofileapi, {
+          headers: {
+            Authorization: `Bearer ${tokenData}`,
+          },
+        });
 
-          const { users } = response.data;
-          console.log("Fetched users:", users);
-          return users;
-        } catch (error) {
-          console.error("Invalid token or error fetching users:", error);
-          navigate("/"); // Redirect to login if error
-        }
+        const { users } = response.data;
+        console.log("Fetched users:", users);
+        return users;
+      } catch (error) {
+        console.log("Inside the error");
+        console.error("Invalid token or error fetching users:", error);
+        navigate("/"); // Redirect to login if error
       }
-    };
+    }
+  };
 
-    checkToken();
-  }, [navigate]);
+  checkToken();
 };
 
 export default useCheckToken;
