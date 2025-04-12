8dfe732be42e6131b159842ff75f20c411e698c6
Archit-KineticScape <archit@kineticscapestudios.com>
2025-04-12 16:40:43 +0530

    Update useCheckToken.js

--- src/hooks/useCheckToken.js
+++ src/hooks/useCheckToken.js
@@ -4,38 +4,37 @@ import { useNavigate } from "react-router";
 import { getnearbyusersapi } from "../common/apis";
 
 const useCheckToken = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const checkToken = async () => {
-            const token = localStorage.getItem("token");
-            const tokenData = JSON.parse(token);
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = localStorage.getItem("token");
+      const tokenData = JSON.parse(token);
 
-            console.log("hook token :", tokenData)
+      console.log("hook token :", tokenData);
 
-            if (!tokenData) {
-                navigate("/");
-            }
-            else {
-                try {
-                    const response = await axios.get(getnearbyusersapi, {
-                        headers: {
-                            Authorization: `Bearer ${tokenData}`,
-                        },
-                    });
+      if (!tokenData) {
+        navigate("/");
+      } else {
+        try {
+          const response = await axios.get(getuserprofileapi, {
+            headers: {
+              Authorization: `Bearer ${tokenData}`,
+            },
+          });
 
-                    const { users } = response.data;
-                    console.log("Fetched users:", users);
-                    return users
-                } catch (error) {
-                    console.error("Invalid token or error fetching users:", error);
-                    navigate("/"); // Redirect to login if error
-                }
-            }
-        };
+          const { users } = response.data;
+          console.log("Fetched users:", users);
+          return users;
+        } catch (error) {
+          console.error("Invalid token or error fetching users:", error);
+          navigate("/"); // Redirect to login if error
+        }
+      }
+    };
 
-        checkToken();
-    }, [navigate]);
+    checkToken();
+  }, [navigate]);
 };
 
 export default useCheckToken;
